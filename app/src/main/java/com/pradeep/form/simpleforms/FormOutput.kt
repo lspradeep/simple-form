2904e38ef9ce2eca1b9882c1450d9bb8f847c8de
Pradeep <lspradeep21@gmail.com>
2021-07-25 15:19:47 +0530

    removed kotlinx-android-extensions and added kotlinx-parcelize

--- app/src/main/java/com/pradeep/form/simpleforms/FormOutput.kt
+++ app/src/main/java/com/pradeep/form/simpleforms/FormOutput.kt
@@ -1,7 +1,7 @@
 package com.pradeep.form.simpleforms
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class FormOutput(
